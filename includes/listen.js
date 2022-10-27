929982f0a563756ec816646f1d77be48941a9705
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2022-10-27 12:04:41 +0700

    Update listen.js

--- includes/listen.js
+++ includes/listen.js
@@ -1,13 +1,101 @@
 module.exports = function({ api, models }) {
 
-	const Users = require("./controllers/users")({ models, api }),
-				Threads = require("./controllers/threads")({ models, api }),
-				Currencies = require("./controllers/currencies")({ models });
-	const logger = require("../utils/log.js");
+  const { ADMINBOT, SUPERADMIN } = global.config;
+ const Users = require("./controllers/users")({ models, api }),
+    Threads = require("./controllers/threads")({ models, api }),
+    Currencies = require("./controllers/currencies")({ models });
+ // const adminbot = require('./../../config.json');
 
-	//////////////////////////////////////////////////////////////////////
-	//========= Push all variable from database to environment =========//
-	//////////////////////////////////////////////////////////////////////
+
+ const logger = require("../utils/log.js");
+ const fs = require("fs");
+ const moment = require('moment-timezone');
+ const axios = require("axios");
+  const request = require("request")
+  var day = moment.tz("Asia/Ho_Chi_Minh").day();
+
+
+   const checkttDataPath = __dirname + '/../modules/commands/_checktuongtac_nghia/';
+  setInterval(async() => {
+      
+    const day_now = moment.tz("Asia/Ho_Chi_Minh").day();
+    if (day != day_now) {
+      day = day_now;
+      const checkttData = fs.readdirSync(checkttDataPath);
+      console.log('--> CHECKTT: Ngày Mới');
+      checkttData.forEach(async(checkttFile) => {
+        const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
+        let storage = [], count = 1;
+        for (const item of checktt.day) {
+            const userName = (await api.getUserInfoV2(item.id)).name || 'Facebook User';
+            const itemToPush = item;
+            itemToPush.name = userName;
+            storage.push(itemToPush);
+        };
+        storage.sort((a, b) => {
+            if (a.count > b.count) {
+                return -1;
+            }
+            else if (a.count < b.count) {
+                return 1;
+            } else {
+                return a.name.localeCompare(b.name);
+            }
+        });
+        let checkttBody = '===Top 10 Tương Tác Ngày===\n';
+        checkttBody += storage.slice(0, 10).map(item => {
+          return `${count++}. ${item.name} (${item.count})`;
+      }).join('\n');
+        api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
+        
+        checktt.day.forEach(e => {
+            e.count = 0;
+        });
+        checktt.time = day_now;
+        
+        fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
+      });
+      if (day_now == 1) {
+        console.log('--> CHECKTT: Tuần Mới');
+        checkttData.forEach(async(checkttFile) => {
+          const checktt = JSON.parse(fs.readFileSync(checkttDataPath + checkttFile));
+          let storage = [], count = 1;
+          for (const item of checktt.week) {
+              const userName = await Users.getNameUser(item.id) || 'Facebook User';
+              const itemToPush = item;
+              itemToPush.name = userName;
+              storage.push(itemToPush);
+          };
+          storage.sort((a, b) => {
+              if (a.count > b.count) {
+                  return -1;
+              }
+              else if (a.count < b.count) {
+                  return 1;
+              } else {
+                  return a.name.localeCompare(b.name);
+              }
+          });
+          let checkttBody = '===Top 10 Tương Tác Tuần===\n';
+          checkttBody += storage.slice(0, 10).map(item => {
+            return `${count++}. ${item.name} (${item.count})`;
+        }).join('\n');
+          api.sendMessage(checkttBody, checkttFile.replace('.json', ''), (err) => err ? console.log(err) : '');
+          checktt.week.forEach(e => {
+              e.count = 0;
+          });
+          
+          fs.writeFileSync(checkttDataPath + checkttFile, JSON.stringify(checktt, null, 4));
+        })
+      }
+      global.client.sending_top = false;
+    }
+  }, 1000 * 10);
+ 
+  
+  //////////////////////////////////////////////////////////////////////
+ //========= Push all variable from database to environment =========//
+ //////////////////////////////////////////////////////////////////////
 	
 (async function () {
 
@@ -22,8 +110,8 @@ module.exports = function({ api, models }) {
             global.data.threadData.set(idThread, data['data'] || {}), 
             global.data.threadInfo.set(idThread, data.threadInfo || {});
             if (data['data'] && data['data']['banned'] == !![]) 
-            	global.data.threadBanned.set(idThread, 
-            	{
+             global.data.threadBanned.set(idThread, 
+             {
                 'reason': data['data']['reason'] || '',
                 'dateAdded': data['data']['dateAdded'] || ''
             });
@@ -49,46 +137,175 @@ module.exports = function({ api, models }) {
         return logger.loader(global.getText('listen', 'failLoadEnvironment', error), 'error');
     }
 }());
-	logger(`${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "This bot was made by CatalizCS and SpermLord" : global.config.BOTNAME}`, "[ BOT INFO ]");
+ logger(`${api.getCurrentUserID()} - [ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? "This bot was made by CatalizCS and SpermLord" : global.config.BOTNAME}`, "[ BOT INFO ]");
 	
-	///////////////////////////////////////////////
-	//========= Require all handle need =========//
-	//////////////////////////////////////////////
+ ///////////////////////////////////////////////
+ //========= Require all handle need =========//
+ //////////////////////////////////////////////
 
-	const handleCommand = require("./handle/handleCommand")({ api, models, Users, Threads, Currencies });
-	const handleCommandEvent = require("./handle/handleCommandEvent")({ api, models, Users, Threads, Currencies });
-	const handleReply = require("./handle/handleReply")({ api, models, Users, Threads, Currencies });
-	const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
-	const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
-	const handleCreateDatabase = require("./handle/handleCreateDatabase")({  api, Threads, Users, Currencies, models });
+ const handleCommand = require("./handle/handleCommand")({ api, models, Users, Threads, Currencies });
+ const handleCommandEvent = require("./handle/handleCommandEvent")({ api, models, Users, Threads, Currencies });
+ const handleReply = require("./handle/handleReply")({ api, models, Users, Threads, Currencies });
+ const handleReaction = require("./handle/handleReaction")({ api, models, Users, Threads, Currencies });
+ const handleEvent = require("./handle/handleEvent")({ api, models, Users, Threads, Currencies });
+ const handleCreateDatabase = require("./handle/handleCreateDatabase")({ api, Threads, Users, Currencies, models });
 
-	logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
+// logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
 
-	//////////////////////////////////////////////////
-	//========= Send event to handle need =========//
-	/////////////////////////////////////////////////
+
+ //DEFINE DATLICH PATH
+ const datlichPath = __dirname + '/../modules/commands/cache/datlich.json';
+
+ //FUNCTION HOẠT ĐỘNG NHƯ CÁI TÊN CỦA NÓ, CRE: DUNGUWU
+ const monthToMSObj = {
+  1: 31 * 24 * 60 * 60 * 1000,
+  2: 28 * 24 * 60 * 60 * 1000,
+  3: 31 * 24 * 60 * 60 * 1000,
+  4: 30 * 24 * 60 * 60 * 1000,
+  5: 31 * 24 * 60 * 60 * 1000,
+  6: 30 * 24 * 60 * 60 * 1000,
+  7: 31 * 24 * 60 * 60 * 1000,
+  8: 31 * 24 * 60 * 60 * 1000,
+  9: 30 * 24 * 60 * 60 * 1000,
+  10: 31 * 24 * 60 * 60 * 1000,
+  11: 30 * 24 * 60 * 60 * 1000,
+  12: 31 * 24 * 60 * 60 * 1000
+ };
+ const checkTime = (time) => new Promise((resolve) => {
+  time.forEach((e, i) => time[i] = parseInt(String(e).trim()));
+  const getDayFromMonth = (month) => (month == 0) ? 0 : (month == 2) ? (time[2] % 4 == 0) ? 29 : 28 : ([1, 3, 5, 7, 8, 10, 12].includes(month)) ? 31 : 30;
+  if (time[1] > 12 || time[1] < 1) resolve("Tháng của bạn có vẻ không hợp lệ");
+  if (time[0] > getDayFromMonth(time[1]) || time[0] < 1) resolve("Ngày của bạn có vẻ không hợp lệ");
+  if (time[2] < 2022) resolve("Bạn sống ở kỷ nguyên nào thế?");
+  if (time[3] > 23 || time[3] < 0) resolve("Giờ của bạn có vẻ không hợp lệ");
+  if (time[4] > 59 || time[3] < 0) resolve("Phút của bạn có vẻ không hợp lệ");
+  if (time[5] > 59 || time[3] < 0) resolve("Giây của bạn có vẻ không hợp lệ");
+  yr = time[2] - 1970;
+  yearToMS = (yr) * 365 * 24 * 60 * 60 * 1000;
+  yearToMS += ((yr - 2) / 4).toFixed(0) * 24 * 60 * 60 * 1000;
+  monthToMS = 0;
+  for (let i = 1; i < time[1]; i++) monthToMS += monthToMSObj[i];
+  if (time[2] % 4 == 0) monthToMS += 24 * 60 * 60 * 1000;
+  dayToMS = time[0] * 24 * 60 * 60 * 1000;
+  hourToMS = time[3] * 60 * 60 * 1000;
+  minuteToMS = time[4] * 60 * 1000;
+  secondToMS = time[5] * 1000;
+  oneDayToMS = 24 * 60 * 60 * 1000;
+  timeMs = yearToMS + monthToMS + dayToMS + hourToMS + minuteToMS + secondToMS - oneDayToMS;
+  resolve(timeMs);
+ });
+
+
+ const tenMinutes = 10 * 60 * 1000;
+
+ logger.loader(`====== ${Date.now() - global.client.timeStart}ms ======`);
+ const checkAndExecuteEvent = async () => {
+
+  /*smol check*/
+  if (!fs.existsSync(datlichPath)) fs.writeFileSync(datlichPath, JSON.stringify({}, null, 4));
+  var data = JSON.parse(fs.readFileSync(datlichPath));
+
+  //GET CURRENT TIME
+  var timeVN = moment().tz('Asia/Ho_Chi_Minh').format('DD/MM/YYYY_HH:mm:ss');
+  timeVN = timeVN.split("_");
+  timeVN = [...timeVN[0].split("/"), ...timeVN[1].split(":")];
+
+  let temp = [];
+  let vnMS = await checkTime(timeVN);
+  const compareTime = e => new Promise(async (resolve) => {
+   let getTimeMS = await checkTime(e.split("_"));
+   if (getTimeMS < vnMS) {
+    if (vnMS - getTimeMS < tenMinutes) {
+     data[boxID][e]["TID"] = boxID;
+     temp.push(data[boxID][e]); delete data[boxID][e];
+    } else delete data[boxID][e];
+    fs.writeFileSync(datlichPath, JSON.stringify(data, null, 4));
+   };
+   resolve();
+  })
+
+  await new Promise(async (resolve) => {
+   for (boxID in data) {
+    for (e of Object.keys(data[boxID])) await compareTime(e);
+   }
+   resolve();
+  })
+  for (el of temp) {
+   try {
+    var all = (await Threads.getInfo(el["TID"])).participantIDs;
+       all.splice(all.indexOf(api.getCurrentUserID()), 1);
+    var body = el.REASON || "MỌI NGƯỜI ƠI", mentions = [], index = 0;
+    
+       for (let i = 0; i < all.length; i++) {
+        if (i == body.length) body += " ‍ ";
+        mentions.push({
+         tag: body[i],
+         id: all[i],
+         fromIndex: i - 1
+        });
+       }
+   } catch (e) { return console.log(e); }
+   var out = {
+    body, mentions
+   }
+   if ("ATTACHMENT" in el) {
+    out.attachment = [];
+    for (a of el.ATTACHMENT) {
+     let getAttachment = (await axios.get(encodeURI(a.url), { responseType: "arraybuffer"})).data;
+     fs.writeFileSync(__dirname + `/../modules/commands/cache/${a.fileName}`, Buffer.from(getAttachment, 'utf-8'));
+     out.attachment.push(fs.createReadStream(__dirname + `/../modules/commands/cache/${a.fileName}`));
+    }
+   }
+   console.log(out);
+   if ("BOX" in el) await api.setTitle(el["BOX"], el["TID"]);
+   api.sendMessage(out, el["TID"], () => ("ATTACHMENT" in el) ? el.ATTACHMENT.forEach(a => fs.unlinkSync(__dirname + `/../modules/commands/cache/${a.fileName}`)) : "");
+  }
+
+ }
+ setInterval(checkAndExecuteEvent, tenMinutes/10);
+/*
+let unsend = __dirname + "/../modules/commands/cache/unsendReaction.json";
+		if (!fs.existsSync(unsend)) fs.writeFileSync(unsend, JSON.stringify({}, null, 4));
+		let unsendData = JSON.parse(fs.readFileSync(unsend));
+		if(!unsendData[event.threadID]) unsendData[event.threadID] = { data: false };
+		fs.writeFileSync(unsend, JSON.stringify(unsendData, null, 4));
+//const id = "100000272507589"
+       // if (id != handleReaction.author) return api.sendMessage("nịt", event.threadID, event.messageID);
+		if(event.type == "message_reaction" && ADMINBOT.includes(event.senderID) && event.senderID == api.getCurrentUserID() && unsendData[event.threadID].data) api.unsendMessage(event.messageID);
+  */
+ //////////////////////////////////////////////////
+ //========= Send event to handle need =========//
+ /////////////////////////////////////////////////
 	
-	return (event) => {
-		switch (event.type) {
-			case "message":
-			case "message_reply":
-			case "message_unsend":
-				handleCreateDatabase({ event });
-				handleCommand({ event });
-				handleReply({ event });
-				handleCommandEvent({ event });
+ return (event) => {
+   if (event.type == "change_thread_image") {
+event.type = "event";
+event.logMessageType = "change_thread_image";
+   }
+//if (event.type == "change_thread_image") api.sendMessage(`» [ CẬP NHẬT NHÓM ] ${event.snippet}`, event.threadID);
+ 
+  if (global.config.ibrieng == true){
+    if(event.body && !event.isGroup && !ADMINBOT.includes(event.senderID) && event.senderID !== api.getCurrentUserID()) {  return api.sendMessage("[ Status ] - on \n[ Warn ] - Sadboiz see tình\n[ Author ] - Brasl Việt Lê", event.threadID, event.messageID) 
+       }
+  } 
+   switch (event.type) {
+   case "message":
+   case "message_reply":
+   case "message_unsend":
+    handleCreateDatabase({ event });
+    handleCommand({ event });
+    handleReply({ event });
+    handleCommandEvent({ event });
 
-				break;
-			case "event":
-				handleEvent({ event });
-				break;
-			case "message_reaction":
-				handleReaction({ event });
-				break;
-			default:
-				break;
-		}
-	};
+    break;
+   case "event":
+    handleEvent({ event });
+    break;
+   case "message_reaction":
+    handleReaction({ event });
+    break;
+   default:
+    break;
+  }
+ };
 };
-
-//THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
