280932cacdd53a8c42c189870f437a9481428e89
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2022-10-27 18:31:58 +0700

    Update leaveNoti.js

--- modules/events/leaveNoti.js
+++ modules/events/leaveNoti.js
@@ -1,52 +1,33 @@
 module.exports.config = {
-	name: "leaveNoti",
+	name: "leave",
 	eventType: ["log:unsubscribe"],
 	version: "1.0.0",
-	credits: "Mirai team",
-	description: "Thông báo bot hoặc người rời khỏi nhóm có random gif/ảnh/video",
+	credits: "Mirai Team",
+	description: "Thông báo bot hoặc người rời khỏi nhóm",
 	dependencies: {
 		"fs-extra": "",
 		"path": ""
 	}
 };
 
-module.exports.onLoad = function () {
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { join } = global.nodemodule["path"];
-
-	const path = join(__dirname, "cache", "leaveGif");
-	if (existsSync(path)) mkdirSync(path, { recursive: true });	
-
-	const path2 = join(__dirname, "cache", "leaveGif", "randomgif");
-    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
-
-    return;
-}
-
 module.exports.run = async function({ api, event, Users, Threads }) {
 	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-	const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
 	const { join } =  global.nodemodule["path"];
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "𝙩ự 𝙧ờ𝙞" : "𝙗ị 𝙦𝙪ả𝙣 𝙩𝙧ị 𝙫𝙞ê𝙣 đá";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "tự rời" : "bị quản trị viên đá";
 	const path = join(__dirname, "cache", "leaveGif");
-	const gifPath = join(path, `${threadID}.gìf`);
+	const gifPath = join(path, `leave.mp4`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = " {name} Đã {type} 𝙠𝙝ỏ𝙞 𝙣𝙝ó𝙢" : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
-
-	const randomPath = readdirSync(join(__dirname, "cache", "leaveGif", "randomgif"));
+	(typeof data.customLeave == "undefined") ? msg = "{name} Đã {type} khỏi nhóm\n\nprofile: https://www.facebook.com/profile.php?id={uid} " : msg = data.customLeave;
+	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{uid}/g, uid);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
-	else if (randomPath.length != 0) {
-		const pathRandom = join(__dirname, "cache", "leaveGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
-		formPush = { body: msg, attachment: createReadStream(pathRandom) }
-	}
 	else formPush = { body: msg }
 	
 	return api.sendMessage(formPush, threadID);
