4283d39930b47738a1404bdb236306f4a727be35
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2022-10-27 15:41:59 +0700

    Create listbox.js

---
+++ modules/commands/listbox.js
@@ -0,0 +1,205 @@
+module.exports.config = {
+    name: 'listbox',
+    version: '1.0.0',
+    credits: 'ManhG',
+    hasPermssion: 3,
+    description: '[Ban/Unban/Remove] List thread bot đã tham gia',
+    commandCategory: 'Dành cho Admin',
+    usages: '[số trang/all]',
+    cooldowns: 5
+};
+
+module.exports.handleReply = async function({ api, event, args, Threads, handleReply }) {
+    const { threadID, messageID } = event;
+    if (parseInt(event.senderID) !== parseInt(handleReply.author)) return;
+    const moment = require("moment-timezone");
+    const time = moment.tz("Asia/Ho_Chi_minh").format("HH:MM:ss L");
+    var arg = event.body.split(" ");
+    //var idgr = handleReply.groupid[arg[1] - 1];
+    //var groupName = handleReply.groupName[arg[1] - 1];
+    switch (handleReply.type) {
+        case "reply":
+            {
+                if (arg[0] == "ban" || arg[0] == "Ban") {
+                    var arrnum = event.body.split(" ");
+                    var msg = "";
+                    var modules = "» Thực thi Ban «\n"
+                    var nums = arrnum.map(n => parseInt(n));
+                    nums.shift();
+                    for (let num of nums) {
+                        var idgr = handleReply.groupid[num - 1];
+                        var groupName = handleReply.groupName[num - 1];
+
+                        const data = (await Threads.getData(idgr)).data || {};
+                        data.banned = true;
+                        data.dateAdded = time;
+                        var typef = await Threads.setData(idgr, { data });
+                        global.data.threadBanned.set(idgr, { dateAdded: data.dateAdded });
+                        msg += typef + ' ' + groupName + '\n» TID: ' + idgr + "\n";
+                        console.log(modules, msg)
+                    }
+                    api.sendMessage(`» Thông báo từ Admin «\n Nhóm Bạn Đã Bị Ban, cấm dùng bot.`, idgr, () =>
+                        api.sendMessage(`${global.data.botID}`, () =>
+                            api.sendMessage(`» Thực thi Ban «(true/false) «\n\n${msg}`, threadID, () =>
+                                api.unsendMessage(handleReply.messageID))));
+                    break;
+                }
+
+                if (arg[0] == "unban" || arg[0] == "Unban" || arg[0] == "ub" || arg[0] == "Ub") {
+                    var arrnum = event.body.split(" ");
+                    var msg = "";
+                    var modules = "» Thực thi Unban «\n"
+                    var nums = arrnum.map(n => parseInt(n));
+                    nums.shift();
+                    for (let num of nums) {
+                        var idgr = handleReply.groupid[num - 1];
+                        var groupName = handleReply.groupName[num - 1];
+
+                        const data = (await Threads.getData(idgr)).data || {};
+                        data.banned = false;
+                        data.dateAdded = null;
+                        var typef = await Threads.setData(idgr, { data });
+                        global.data.threadBanned.delete(idgr, 1);
+                        msg += typef + ' ' + groupName + '\n» TID: ' + idgr + "\n";
+                        console.log(modules, msg)
+                    }
+                    api.sendMessage(`» Thông báo từ Admin «\n\n Nhóm Bạn Đã Được Gỡ Ban`, idgr, () =>
+                        api.sendMessage(`${global.data.botID}`, () =>
+                            api.sendMessage(`» Thực thi Unban «(true/false)\n\n${msg}`, threadID, () =>
+                                api.unsendMessage(handleReply.messageID))));
+                    break;
+                }
+
+                if (arg[0] == "out" || arg[0] == "Out") {
+                    var arrnum = event.body.split(" ");
+                    var msg = "";
+                    var modules = "» Thực thi Out «\n"
+                    var nums = arrnum.map(n => parseInt(n));
+                    nums.shift();
+                    for (let num of nums) {
+                        var idgr = handleReply.groupid[num - 1];
+                        var groupName = handleReply.groupName[num - 1];
+                        var typef = api.removeUserFromGroup(`${api.getCurrentUserID()}`, idgr);
+                        msg += typef + ' ' + groupName + '\n» TID: ' + idgr + "\n";
+                        console.log(modules, msg)
+                    }
+                    api.sendMessage(`» Thông báo từ Admin «\n\nTạm biệt nhé\nTớ out đây😢`, idgr, () =>
+                        api.sendMessage(`${global.data.botID}`, () =>
+                            api.sendMessage(`★★Thực thi Out(true/false)★★\n\n${msg} `, threadID, () =>
+                                api.unsendMessage(handleReply.messageID))));
+                    break;
+                }
+            }
+    }
+};
+module.exports.run = async function({ api, event, args }) {
+  const permission = ["", "","100036576590462"];
+	if (!permission.includes(event.senderID)) return api.sendMessage("cút:))", event.threadID, event.messageID);
+    switch (args[0]) {
+        case "all":
+            {
+                var inbox = await api.getThreadList(100, null, ['INBOX']);
+                let list = [...inbox].filter(group => group.isSubscribed && group.isGroup);
+                var listthread = [];
+                var listbox = [];
+                /////////
+                for (var groupInfo of list) {
+                    //let data = (await api.getThreadInfo(groupInfo.threadID));
+                    //const listUserID = event.participantIDs.filter(ID => ID);
+                    listthread.push({
+                        id: groupInfo.threadID,
+                        name: groupInfo.name || "Chưa đặt tên",
+                        participants: groupInfo.participants.length
+                    });
+                }
+                /////////
+                var listbox = listthread.sort((a, b) => {
+                    if (a.participants > b.participants) return -1;
+                    if (a.participants < b.participants) return 1;
+                });
+                /////////  
+                var groupid = [];
+                var groupName = [];
+                var page = 1;
+                page = parseInt(args[0]) || 1;
+                page < -1 ? page = 1 : "";
+                var limit = 100000;
+                var msg = "» DS NHÓM ĐÃ THAM GIA «\n\n";
+                var numPage = Math.ceil(listbox.length / limit);
+
+                for (var i = limit * (page - 1); i < limit * (page - 1) + limit; i++) {
+                    if (i >= listbox.length) break;
+                    let group = listbox[i];
+                    msg += `${i + 1}. ${group.name}\n🔰 TID: ${group.id}\n👤 Số thành viên: ${group.participants}\n\n`;
+                    groupid.push(group.id);
+                    groupName.push(group.name);
+                }
+                msg += `\n--Trang ${page}/${numPage}--\nDùng ${global.config.PREFIX}listbox all + số trang\n\n`
+
+                api.sendMessage(msg + '🎭 Reply Out, Ban, Unban + số thứ tự, có thể rep nhiều số, cách nhau bằng dấu cách để Out, Ban, Unban thread đó!', event.threadID, (e, data) =>
+                    global.client.handleReply.push({
+                        name: this.config.name,
+                        author: event.senderID,
+                        messageID: data.messageID,
+                        groupid,
+                        groupName,
+                        type: 'reply'
+                    })
+                )
+            }
+            break;
+
+        default:
+            try {
+                var inbox = await api.getThreadList(100, null, ['INBOX']);
+                let list = [...inbox].filter(group =>  group.isSubscribed && group.isGroup);
+                var listthread = [];
+                var listbox = [];
+                /////////
+                for (var groupInfo of list) {
+                    //let data = (await api.getThreadInfo(groupInfo.threadID));
+                    //const listUserID = event.participantIDs.filter(ID => ID);
+                    listthread.push({
+                        id: groupInfo.threadID,
+                        name: groupInfo.name || "Chưa đặt tên",
+                        participants: groupInfo.participants.length
+                    });
+
+                } //for
+                var listbox = listthread.sort((a, b) => {
+                    if (a.participants > b.participants) return -1;
+                    if (a.participants < b.participants) return 1;
+                });
+                var groupid = [];
+                var groupName = [];
+                var page = 1;
+                page = parseInt(args[0]) || 1;
+                page < -1 ? page = 1 : "";
+                var limit = 100;
+                var msg = "» DS NHÓM ĐÃ THAM GIA «\n\n";
+                var numPage = Math.ceil(listbox.length / limit);
+
+                for (var i = limit * (page - 1); i < limit * (page - 1) + limit; i++) {
+                    if (i >= listbox.length) break;
+                    let group = listbox[i];
+                    msg += `${i + 1}. ${group.name}\n🔰TID: ${group.id}\n👤 Số thành viên: ${group.participants}\n\n`;
+                    groupid.push(group.id);
+                    groupName.push(group.name);
+                }
+                msg += `--Trang ${page}/${numPage}--\nDùng ${global.config.PREFIX}listbox + số trang/all\n\n`
+
+                api.sendMessage(msg + '🎭 Reply Out, Ban, Unban + số thứ tự, có thể rep nhiều số, cách nhau bằng dấu cách để Out, Ban, Unban thread đó!', event.threadID, (e, data) =>
+                    global.client.handleReply.push({
+                        name: this.config.name,
+                        author: event.senderID,
+                        messageID: data.messageID,
+                        groupid,
+                        groupName,
+                        type: 'reply'
+                    })
+                )
+            } catch (e) {
+                return console.log(e)
+            }
+    }
+};
