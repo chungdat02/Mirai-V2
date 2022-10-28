3ceebb9d5a716856303d13cd07e0e260e1350db4
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2022-10-28 14:20:37 +0700

    Update setprefix.js

--- modules/commands/setprefix.js
+++ modules/commands/setprefix.js
@@ -33,7 +33,10 @@ module.exports.handleReaction = async function({ api, event, Threads, handleReac
 		await Threads.setData(threadID, { data });
 		await global.data.threadData.set(String(threadID), data);
 		api.unsendMessage(handleReaction.messageID);
+    
+     api.changeNickname(`[ ${handleReaction.PREFIX} ] • ${global.config.BOTNAME}`, event.threadID, event.senderID);
 		return api.sendMessage(getText("successChange", handleReaction.PREFIX), threadID, messageID);
+    
 	} catch (e) { return console.log(e) }
 }
 
@@ -46,6 +49,9 @@ module.exports.run = async ({ api, event, args, Threads , getText }) => {
 		data["PREFIX"] = global.config.PREFIX;
 		await Threads.setData(event.threadID, { data });
 		await global.data.threadData.set(String(event.threadID), data);
+    var uid = api.getCurrentUserID()
+    api.changeNickname(`[ ${global.config.PREFIX} ] • ${global.config.BOTNAME}`,event.threadID, uid);
+    
 		return api.sendMessage(getText("resetPrefix", global.config.PREFIX), event.threadID, event.messageID);
 	} else return api.sendMessage(getText("confirmChange", prefix), event.threadID, (error, info) => {
 		global.client.handleReaction.push({
