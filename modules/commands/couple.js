e42242d01c4999e621b13eca05517d8a11672828
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2022-12-29 16:47:57 +0700

    Create couple.js

---
+++ modules/commands/couple.js
@@ -0,0 +1,37 @@
+module.exports.config = {
+    name: "couple",
+    version: "beta",
+    hasPermssion: 0,
+    credits: "D",
+    description: "Cao Chung Đạt",
+    commandCategory: "tien ich",
+    usages: "uhmmmm",
+    cooldowns: 1
+};
+
+module.exports.run = async function ({event, api, Currencies, Threads}){
+  const {messageID, threadID} = event;
+  const 
+    axios = require('axios'),
+    fs = require('fs');
+  try {
+const res = await axios.get(`https://caochungdat.me/docs/images/couple`),
+      res1 = await axios.get(`https://caochungdat.me/docs/other/thinh`),
+      bokk = res1.data.url,
+      url = [];
+    for (var i in res.data.url) {
+const path = __dirname+`/cache/couple/${i}.jpg`, ///check đoạn này giúp a nha Nam 
+      bok = (await axios.get(`${res.data.url[i]}`, { responseType: 'arraybuffer' })).data;
+      fs.writeFileSync(path, bok);
+      url.push(fs.createReadStream(path));
+    }
+    ////// end cái for ko biết chạy dc ko
+    return api.sendMessage({
+      attachment: url,
+      body: `=== ảnh couple của bạn đây ===\n${bokk}`
+    }, threadID, messageID);
+  }
+  catch (err){
+    return api.sendMessage(`loi `, event.threadID);
+  }
+};
