a720764d8ddcdeedf04dc447dd9438bca2749406
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2023-01-04 00:25:13 +0700

    Update couple.js

--- modules/commands/couple.js
+++ modules/commands/couple.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "couple",
     version: "beta",
     hasPermssion: 0,
-    credits: "D",
+    credits: "",
     description: "Cao Chung Đạt",
     commandCategory: "tien ich",
     usages: "uhmmmm",
@@ -20,7 +20,7 @@ const res = await axios.get(`https://caochungdat.me/docs/images/couple`),
       bokk = res1.data.url,
       url = [];
     for (var i in res.data.url) {
-const path = __dirname+`/cache/couple/${i}.jpg`, ///check đoạn này giúp a nha Nam 
+const path = __dirname+`/cache/couple/${i}.jpg`, ///done 
       bok = (await axios.get(`${res.data.url[i]}`, { responseType: 'arraybuffer' })).data;
       fs.writeFileSync(path, bok);
       url.push(fs.createReadStream(path));
