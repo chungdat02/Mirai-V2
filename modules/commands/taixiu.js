2cf0f4cae19ba523a07a1f6b9b1dff418b5b8e4b
Cao Chung Đạt <116594266+chungdat02@users.noreply.github.com>
2023-06-07 13:29:41 +0700

    Update taixiu.js

--- modules/commands/taixiu.js
+++ modules/commands/taixiu.js
@@ -10,20 +10,22 @@ module.exports.config = {
 };
 module.exports.run = async function ({ api, event, args, Currencies, Users }) {
     const { senderID, messageID, threadID } = event;
-    const axios = require('axios');
-    const fs = require("fs-extra");
+    const axios = require('axios'),
+           fs = require("fs-extra");
     const dataMoney = await Currencies.getData(senderID);
     const moneyUser = dataMoney.money;
     if (!args[0]) return api.sendMessage("Bạn phải cược tài hoặc xỉu...ngu mà cứ thích cờ bạc", threadID, messageID);
     const choose = args[0]
     if (choose != 'tài' && choose != 'xỉu') return api.sendMessage("Chỉ đặt cược tài hoặc xỉu!", threadID, messageID)
-    const money = args[1]
+    var money = args[1];
+    if(money == 'all' || money == 'tất')money = moneyUser;
     if (money < 30 || isNaN(money)) return api.sendMessage("Mức đặt cược của bạn không phù hợp hoặc dưới 30$!!!", threadID, messageID);
     if (moneyUser < money) return api.sendMessage(`⚡️Số dư bạn không đủ ${money}$ để có thể chơi`, threadID, messageID);
     try {
         const res = await axios.get(`https://caochungdat.me/docs/game/taixiu`);
         const images = [];
         const result = res.data.result;
+        const total = res.data.total;
         for (var i in res.data.images) {
   let path = __dirname + `/cache/${i}.png`;
   let imgs = (await axios.get(`${res.data.images[i]}`, { responseType: "arraybuffer" })).data;
@@ -34,13 +36,13 @@ module.exports.run = async function ({ api, event, args, Currencies, Users }) {
   await Currencies.increaseMoney(senderID, parseInt(money * 2));
   api.sendMessage({
     attachment: images,
-      body: `Bạn đã thắng \nĐược: ${money*2}$\nKết quả: ${result}`},threadID, messageID);
+      body: `•Kết quả: ${result} --\n •Tổng: ${total} nút\n•Bạn đã thắng \nĐược: ${money*2}$`},threadID, messageID);
   }
 else {
         await Currencies.decreaseMoney(senderID, parseInt(money));
   api.sendMessage({
     attachment: images,
-      body: `Bạn đã thua\nMất: ${money}$\nKết quả: ${result} `},threadID, messageID);
+      body: `•Kết quả: ${result} -- •Tổng: ${total} nút\n•Bạn đã thua\nMất: ${money}$ `},threadID, messageID);
   for(let i = 0; i < images.length; i++) {
             fs.unlinkSync(__dirname + `/cache/${i}.png`);
   }
